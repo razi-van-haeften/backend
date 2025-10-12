51512a1d857adce535e626d113d47c5758a40edc
razi-van-haeften <razi.vanhaeften@ntschools.net>
2025-10-12 20:38:22 +0930

    stupid mistake

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { Player } from './Player.js';
 
 const app = express();
 const server = createServer(app);
