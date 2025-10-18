8d9f7ac1d83f7d82696b7e2a39815e8ab5117bb3
razi-van-haeften <razi.vanhaeften@ntschools.net>
2025-10-18 13:49:03 +0930

    packet

--- managers/SocketManager.js
+++ managers/SocketManager.js
@@ -11,34 +11,25 @@ export class SocketManager {
 
     registerEvents() {
         this.io.on("connection", (socket) => {
-            console.log("Client connected:", socket.id);
+            console.log("client connected:", socket.id);
 
-            socket.emit("joined", "joined lobby");
-
-            socket.on("join", (name) => this.handleJoin(socket, name));
-            socket.on("chat", (msg) => this.chat.handleChat(socket, msg));
-            socket.on("disconnect", () => this.handleDisconnect(socket));
+            // socket.on("join", (name) => this.handleJoin(socket, name));
+            // socket.on("chat", (msg) => this.chat.handleChat(socket, msg));
+            // socket.on("disconnect", () => this.handleDisconnect(socket));
             socket.on("packet", (buffer) => this.handlePacket(socket, buffer));
         });
     }
 
-    handleJoin(socket, name) {
-        const player = this.players.add(socket.id, name);
-        console.log(`${player.name} joined the game`);
-        socket.broadcast.emit("message", `${player.name} has joined the game`);
-        socket.emit("message", "joined game");
-    }
-
-    handleDisconnect(socket) {
-        const player = this.players.get(socket.id);
-        if (!player) {
-            console.log(`${socket.id} disconnected`);
-            return;
-        }
-        console.log(`${player.name} disconnected`);
-        this.io.emit("message", `${player.name} left the game`);
-        this.players.remove(socket.id);
-    }
+    // handleDisconnect(socket) {
+    //     const player = this.players.get(socket.id);
+    //     if (!player) {
+    //         console.log(`${socket.id} disconnected`);
+    //         return;
+    //     }
+    //     console.log(`${player.name} disconnected`);
+    //     this.io.emit("message", `${player.name} left the game`);
+    //     this.players.remove(socket.id);
+    // }
 
     joinNotification(socket, name){
         const type = Buffer.from([4]);
@@ -53,12 +44,21 @@ export class SocketManager {
         this.joinNotification(socket, player.name);
     }
 
+    handleChat(socket, payload) {
+        const player = this.players.get(socket.id);
+        const type = payload.readUInt8(0);
+        const message = buffer.slice(1).toString('utf8');
+        const sender = player.name;
+        console.log(`${player.name} said ${message}`);
+    }
+
     handlePacket(socket, buffer) {
         const type = buffer.readUInt8(0);
         const payload = buffer.slice(1);
 
         switch (type) {
-            case 0: this.handleJoinGame(socket, payload); break;
+            case 0: this.handleJoinGame(socket, payload); break;    //client send name and joins
+            case 5: this.handleChat(socket, payload); break;        //clint sends chat
         }
     }
 
